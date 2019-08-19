175c6f30aeeeac066eccfed41b98324cf11bb519
Marsha <marshareed11860@gmail.com>
2019-08-19 14:40:35 -0500

    Lab re.sh error if virtual file not exist in virtual-host

--- re.sh
+++ re.sh
@@ -1,11 +1,50 @@
 #!/bin/bash
+
 CONFIG="$1"
 COMMAND="$2"
-if [ $# -ne 2 ]
+
+# Does the first parameter an actual virtual-host
+FILEMATCH=false
+
+# A concatenated string of virtual hosts
+VALID_VHOSTS=''
+
+# Grab a list of all virtual-host files
+VHOSTS=/etc/apache2/sites-available/*.conf
+
+# Loop through the all files in the sites-avaliable directory
+# Build a list of filenames to display in the error message
+# If we find a match set $FILEMATCH to true and stop build the list
+for FILENAME in $VHOSTS
+do
+
+  VHOST="${FILENAME:29:-5}"
+  # Add each virtual-host in the sites-available directory to 
+  # the VHOSTS string. This will provide user feedback if there
+  # is an error
+  if [ -z  "$VALID_VHOSTS" ]
+    then
+      VALID_VHOSTS="\n * $VHOST"
+    else
+      VALID_VHOSTS="${VALID_VHOSTS}\n * $VHOST"
+    fi
+
+  if [ "$FILENAME" == "/etc/apache2/sites-available/${CONFIG}.conf" ]
+  then
+    # Set $FILEMATCH to true if one of those files matches an actual
+    # virtual-host configuration and break the loop
+    FILEMATCH=true
+    break
+  fi
+done
+
+# We could not match the firstargument to a virtual-host preset the user with an error
+if [ $FILEMATCH  == false ]
 then
-    echo ERROR: "$0 requires two parameters {virtual-host} {restart|reload}"
+    echo -e "\e[31mERROR:\e[0m \e[1m\e[33m${CONFIG}\e[0m \e[31mis NOT a valid virtual-host\e[0m \nPlease choose from the following ${VALID_VHOSTS}"
     exit 1
 fi
+
 if [ "$COMMAND" == "reload" ] || [ "$COMMAND" == "restart" ]
 then
     # Move the current execution state to the proper directory
@@ -19,6 +58,6 @@ then
     sudo a2ensite "$CONFIG"
     sudo service apache2 "$COMMAND"
 else
-    echo "ERROR: $COMMAND is an invalid service command {restart|reload}"
+    echo -e "\e[31mERROR:\e[0m \e[1m\e[33m${COMMAND}\e[0m \e[31mis NOT a valid service command\e[0m \nPlease choose from the following \n * restart \n * reload"
     exit 1
 fi
