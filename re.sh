8c333c59ab566f2947d99ff613f565d5fa78e7be
Marsha <marshareed11860@gmail.com>
2019-08-19 12:01:46 -0500

    Added the ability to reject unauthorized service directives

--- re.sh
+++ re.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 CONFIG="$1"
 COMMAND="$2"
-if [ $# -ne 2 ]
+if [ "$COMMAND" == "reload" ] || [ "$COMMAND" == "restart" ]
 then
-    echo "$0 requires two parameters {virtual-host} {restart|reload}"
+    # Move the current execution state to the proper directory
+    cd /etc/apache2/sites-available
+
+    # Disable a vhost configuration
+    sudo a2dissite "$CONFIG"
+    sudo service apache2 "$COMMAND"
+
+    # Enable a vhost configuration
+    sudo a2ensite "$CONFIG"
+    sudo service apache2 "$COMMAND"
+else
+    echo "ERROR: $COMMAND is an invalid service command {restart|reload}"
     exit 1
 fi
-# Move the current execution state to the proper directory
-cd /etc/apache2/sites-available
-
-# Disable a vhost configuration
-sudo a2dissite "$CONFIG"
-sudo service apache2 "$COMMAND"
-
-# Enable a vhost configuration
-sudo a2ensite "$CONFIG"
-sudo service apache2 "$COMMAND"
